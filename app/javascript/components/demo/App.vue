b8adb278a0a6785ca1a85ae298552159658755bb
mnuma <mh10hiroaki@gmail.com>
2018-11-02 10:19:05 +0900

    using vue

---
+++ app/javascript/components/demo/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="vue-app">
+    <p class="hoge">{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p.hoge {
+  color: red;
+}
+</style>
---
+++ app/javascript/packs/demo/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from '../../components/demo/App.vue'
+
+Vue.component('vue-app', App)
+
+new Vue({
+  el: '#app'
+})
